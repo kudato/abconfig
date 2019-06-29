550ccab8d42c63f06a213f5029665699f041a543
Alexander Shevchenko <kudato@me.com>
2019-06-29 19:15:32 +0300

    Setup path to file via environment variables by default.

--- abconfig/__init__.py
+++ abconfig/__init__.py
@@ -1,5 +1,6 @@
 __version__ = '1.0.0'
 
+from os import environ
 
 from abconfig.common import Dict
 from abconfig.utils import Attrs, Finalize
@@ -8,8 +9,8 @@ from abconfig.env import Env
 
 
 class ABConfig(Dict):
+    load_file = environ.get('CONFIG_FILE', None)
     load_env = False
-    load_file = None
 
     def __init__(self):
         if str(type(self).__name__) == 'ABConfig':
