6c87dfe345c72a8ccb02f858dec7e2e385e539db
Alexander Shevchenko <kudato@me.com>
2019-11-27 01:43:04 +0300

    Some optimizations.

--- abconfig/file.py
+++ abconfig/file.py
@@ -10,25 +10,24 @@ try:
 except ImportError:
     pass
 
+from os.path import splitext
 from abconfig.common import Dict
 
 
 class Reader(Dict):
     def __init__(self, obj: Dict):
-        self._obj = obj
-        path = obj.get('__file__', False)
-        if path != False:
-            super().__init__(obj + self._read(path))
+        self._path = obj.get('__file__', False)
+        if self._path != False:
+            super().__init__(obj + self._read())
         else:
             super().__init__(obj)
 
-    def _read(self, path: str) -> dict:
+    def _read(self) -> dict:
         try:
-            with open(path, 'r') as fd:
-                result = self._reader(fd)
+            with open(self._path, 'r') as fd:
+                result = self._driver(fd)
                 if not isinstance(result, (dict, Dict)):
                     raise IOError
-                self._obj.pop('__file__', False)
                 return result
         except Exception:
             return self.__mempty__
@@ -39,16 +38,22 @@ class Reader(Dict):
 # Drivers:
 
 class Yaml(Reader):
+    __extensions__ = ('yml', 'yaml')
+
     def _driver(self, fd) -> dict:
         return yaml.load(fd, Loader=yaml.FullLoader)
 
 
 class Json(Reader):
+    __extensions__ = ('json',)
+
     def _driver(self, fd) -> dict:
         return json.load(fd)
 
 
 class Toml(Reader):
+    __extensions__ = ('toml',)
+
     def _driver(self, fd) -> dict:
         return toml.load(fd)
 
@@ -57,4 +62,16 @@ class File(Dict):
     __formats__ = (Json, Yaml, Toml)
 
     def __init__(self, obj: Dict):
-        super().__init__(obj.do(*self.__formats__))
+        self._path = obj.get('__file__', False)
+        if self._path != False:
+            super().__init__(obj.do(*self._format()))
+        else:
+            super().__init__(obj)
+
+    def _format(self) -> tuple:
+        _, extension = splitext(self._path)
+        for format_ in self.__formats__:
+            for e in format_.__extensions__:
+                if e == extension[1:]:
+                    return (format_,)
+        return self.__formats__
